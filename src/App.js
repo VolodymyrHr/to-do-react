b5a85e9e44395948e81d23bc20793c49f3d4b78c
volodymyr-hrytsenko <volodyk753@gmail.com>
2020-09-17 17:06:51 +0300

    static to do structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <form name="task">
+            <input type="checkbox" name="done" id="done" value="true"/>
+            <input type="text" name="name" placeholder="Whot to do?" autoFocus/>
+            <button type="submit"><i className="fa fa-plus"></i></button>
+        </form>
+
+        <ul id="tasks">
+          <li className="task">
+            <input id="task-8" type="checkbox" name="list-one" value="true"/>
+            <label htmlFor="task-8">Do homework</label>
+            <button>
+              <i id="delete-8" className="fa fa-times"></i>
+            </button>
+          </li>
+        </ul>
+    </main>
     </div>
   );
 }
